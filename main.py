d39d0ac1f0cd45e1f51f4ea1376ea803c9366a35
reeeverb <skydoessoftware@gmail.com>
2023-12-21 22:06:59 -0600

    colors weeeee

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+def draw_tile(x,y,terrain):
+    terrain_to_color = {"Mountains":"gray","Hills":"red","Forest":"darkgreen","Fields":"yellow","Pasture":"yellowgreen","Desert":"navajowhite"}
+    color = terrain_to_color[terrain]
+    pygame.draw.polygon(screen,color,[(x,y),(x-50,y-25),(x-50,y-75),(x,y-100),(x+50,y-75),(x+50,y-25)])
+
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -15,8 +21,13 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("aqua")
-    pygame.draw.polygon(screen,"black",[(200,175),(150,150),(150,100),(200,75),(250,100),(250,150)])
-
+    #pygame.draw.polygon(screen,"black",[(200,175),(150,150),(150,100),(200,75),(250,100),(250,150)])
+    draw_tile(200,175,"Mountains")
+    draw_tile(250,175,"Hills")
+    draw_tile(300,175,"Forest")
+    draw_tile(350,175,"Fields")
+    draw_tile(400,175,"Pasture")
+    draw_tile(450,175,"Desert")
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
